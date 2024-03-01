cfde8bf9793497830bfac8fc697bed8f70155783
Jon Shamir <jon.shamir@gmail.com>
2024-03-01 17:38:49 +0200

    Add leva controls

--- src/components/ShaderTest/ShaderTest.tsx
+++ src/components/ShaderTest/ShaderTest.tsx
@@ -1,8 +1,10 @@
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
 import { Mesh } from "three";
+import { useControls } from "leva";
 
 import vertexShader from "./basic.vert";
 import fragmentShader from "./test.frag";
+import { useFrame } from "@react-three/fiber";
 
 interface ShaderTestProps {
   position: [number, number, number];
@@ -11,12 +13,35 @@ interface ShaderTestProps {
 export function ShaderTest(props: ShaderTestProps) {
   const meshRef = useRef<Mesh>(null!);
 
+  const controls = useControls({
+    u_radius: { value: 0.5, min: 0, max: 1, step: 0.01 },
+  });
+
+  const uniforms = useMemo(
+    () => ({
+      u_time: {
+        value: 0.0,
+      },
+      u_radius: {
+        value: controls.u_radius,
+      },
+    }),
+    []
+  );
+
+  useFrame((state) => {
+    const { clock } = state;
+    meshRef.current.material.uniforms.u_time.value = clock.getElapsedTime();
+    meshRef.current.material.uniforms.u_radius.value = controls.u_radius;
+  });
+
   return (
     <mesh {...props} ref={meshRef}>
       <boxGeometry />
       <shaderMaterial
         fragmentShader={fragmentShader}
         vertexShader={vertexShader}
+        uniforms={uniforms}
       />
     </mesh>
   );
--- src/components/ShaderTest/test.frag
+++ src/components/ShaderTest/test.frag
@@ -3,9 +3,10 @@
 precision mediump float;
 
 varying vec2 vUv;
+uniform float u_time;
+uniform float u_radius;
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
-uniform float u_time;
 
 vec3 colorA = vec3(0.912,0.191,0.652);
 vec3 colorB = vec3(1.000,0.777,0.052);
@@ -24,10 +25,15 @@ float sdCircle(in vec2 p, in float r)
 float circle(in vec2 p)
 {
     float d = length(p - vec2(0.5, 0.5));
-    float baseRadius = 0.1;
-    float pixelRadius = fwidth(d) * 0.5;
-    float radius = max(baseRadius, pixelRadius * 2.0);
-    return (1.0 - smoothstep(radius - pixelRadius, radius + pixelRadius, d)) * (baseRadius / radius);
+    float baseRadius = u_radius;
+    float pixelRadius = fwidth(d) * 0.5; // Derivative of the distance field
+    float finalRadius = max(baseRadius, pixelRadius * 2.0);
+    float radiusAA = pixelRadius * 1.5;
+    float circle = 1.0 - smoothstep(finalRadius - radiusAA, finalRadius + radiusAA, d);
+    // circle *= clamp(baseRadius / finalRadius, 0.0, 1.0); // Correct for the difference in area
+
+    // float circle = 1.0 - step(finalRadius, d);
+    return circle;
 }
 
 
